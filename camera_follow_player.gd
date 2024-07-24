79aabb0a458f075a3443ca2039aaeb2dc20c0a04
Jeff Manning <info@jeffmanning.ca>
2024-07-23 22:20:02 -0400

    started movement fixes to track mouse.

--- camera_follow_player.gd
+++
@@ -1,5 +0,0 @@
-extends Camera3D
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	look_at(get_parent().position)
--- manage_player.gd
+++ manage_player.gd
@@ -1,12 +1,17 @@
 extends CharacterBody3D
 
 enum {IDLE, WALK, WALK_REVERSE, RUN};
-var speed = 1;
+@export var speed = 5.0
+@export var acceleration = 4.0
+@export var jump_speed = 8.0
 var rotation_increment = 0.2;
 var running = false;
 
 var current_state = IDLE;
 @onready var pc_node = get_node("PC/AnimationPlayer");
+
+@export var mouse_sensitivity = 0.0015
+@onready var spring_arm = $SpringArm3D
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
@@ -17,26 +22,27 @@ func _ready():
 	pc_node.get_animation("running").loop = true;
 
 func _physics_process(delta):
-	var forward = global_transform.basis.z;
-	match current_state:
-		IDLE:
-			speed = 0;
-			pc_node.play("idle");
-		WALK:
-			speed = 1;
-			velocity = forward * speed;
-		WALK_REVERSE:
-			speed = -1;
-			velocity = forward * speed;
-		
-	if(running && current_state != IDLE):
+	velocity.y += -gravity * delta
+	get_move_input(delta)
+	
+	if(current_state == IDLE):
+		pc_node.play("idle");
+	elif(running):
 		speed *= 4;
 		pc_node.play("running");
 	else:
 		pc_node.play("walking");
 	
-	velocity = forward * speed;
 	move_and_slide();
+	
+func get_move_input(delta):
+	var vy = velocity.y
+	velocity.y = 0
+	var input = Input.get_vector("left", "right", "forward", "back")
+	var dir = Vector3(input.x, 0, input.y).rotated(Vector3.UP, spring_arm.rotation.y)
+	velocity = lerp(velocity, dir * speed, acceleration * delta)
+	velocity.y = vy
+
 
 func _input(event):
 	if (Input.is_action_pressed("move_fwd")):
@@ -45,14 +51,15 @@ func _input(event):
 		current_state = WALK_REVERSE;
 	else:
 		current_state = IDLE;
-		
-	if (Input.is_action_pressed("turn_left")):
-		rotate_y(rotation_increment);
-	if (Input.is_action_pressed("turn_right")):
-		rotate_y(-rotation_increment)
+
 		
 	if (Input.is_action_pressed("run")):
 		running = true;
 	else:
 		running = false;
 	
+func _unhandled_input(event):
+	if event is InputEventMouseMotion:
+		spring_arm.rotation.x -= event.relative.y * mouse_sensitivity
+		spring_arm.rotation_degrees.x = clamp(spring_arm.rotation_degrees.x, -90.0, 30.0)
+		spring_arm.rotation.y -= event.relative.x * mouse_sensitivity
